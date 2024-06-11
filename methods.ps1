150e9c04211cecde56117fa04105c52a330bcb99
Willy Angga <ampedwilly@gmail.com>
2024-06-11 15:52:58 +0700

    Update code to take in objects

--- methods.ps1
+++ methods.ps1
@@ -1,14 +1,14 @@
 function Create-Configuration {
   [CmdletBinding()]
   param(
-    [Parameter(Mandatory, ValueFromPipeline)]
+    [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
     [string]$Name,
-    [Parameter()]
+    [Parameter(ValueFromPipelineByPropertyName)]
     [string]$Version="1.0.0",
     [Parameter(Mandatory)]
     [string]$Path,
-    [Parameter()]
-    [ValidateSet("MacOS", "Linux")]
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [ValidateSet("MacOS", "Linux", "Windows")]
     [string]$OperatingSystem="MacOS"
   )
 
@@ -26,9 +26,11 @@ function Create-Configuration {
       New-Item -Path $Path -Name "$($Name).cfg" -ItemType File -ErrorAction Stop
       $Version | Out-File -FilePath "$Path/$($Name).cfg" -Force
       $OperatingSystem | Out-File -FilePath "$Path/$($Name).cfg" -Append -Force
+      Write-Verbose "Configurations created for $Name"
       $CounterPassed++
     } catch {
       Write-Output $_.Exception.Message
+      Write-Verbose "Failed creating config for $Name"
       $CounterFailed++
     }
     Write-Debug "Configurations created: $CounterPassed"
@@ -45,5 +47,16 @@ function Create-Configuration {
 
 $ConfigPath="/Users/willy/Documents/powershell-tutorial/Configs"
 
-$Names = @('TestConfig1', 'TestConfig2', 'TestConfig3')
-$Names | Create-Configuration -Path $ConfigPath -Verbose -Debug
+# $Names = @('TestConfig1', 'TestConfig2', 'TestConfig3')
+# $Names | Create-Configuration -Path $ConfigPath -Verbose -Debug
+
+# $IISServer = New-Object -TypeName PSCustomObject
+# Add-Member -InputObject $IISServer -MemberType NoteProperty -Name "Name" -Value "IISServer2024"
+# Add-Member -InputObject $IISServer -MemberType NoteProperty -Name "Version" -Value "1.0.3"
+# Add-Member -InputObject $IISServer -MemberType NoteProperty -Name "OperatingSystem" -Value "Windows"
+
+# $IISServer | Create-Configuration -Path $ConfigPath
+
+$Servers = Import-Csv -Path "/Users/willy/Documents/powershell-tutorial/servers.csv" -Delimiter ','
+
+$Servers | Create-Configuration -Path $ConfigPath -Verbose
